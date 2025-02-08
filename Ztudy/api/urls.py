8a56cff59cc7baf8aca8ce155b8cb60e0d5eac7d
Bảo Bùi <hongbao2003@gmail.com>
2025-02-09 00:49:29 +0700

    Creating More Complex Views

--- Ztudy/api/urls.py
+++ Ztudy/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('users/', views.UserListCreate.as_view(), name='user-view-create'),
+    path('users/<int:pk>/', views.UserRetrieveUpdateDestroy.as_view(), name='user-view-detail'),
 ]
--- Ztudy/api/views.py
+++ Ztudy/api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import User
 from .serializers import UserSerializer
 
@@ -7,3 +10,25 @@ from .serializers import UserSerializer
 class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def delete(self, request, *args, **kwargs):
+        User.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'pk'
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        username = request.query_params.get('username', None)
+        if username is not None:
+            user = User.objects.filter(username=username)
+        else:
+            user = User.objects.all()
+
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
