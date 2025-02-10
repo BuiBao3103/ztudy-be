c7ec4099e349f4f955a1a29f12401e6b3643416d
buibao3103 <hongbao2003@gmail.com>
2025-02-10 11:36:28 +0700

    add swagger

--- Ztudy/api/urls.py
+++ Ztudy/api/urls.py
@@ -1,7 +1,25 @@
-from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, include
 from . import views
+
+# Set up Swagger Schema View
+schema_view = get_schema_view(
+    openapi.Info(
+        title="User API",
+        default_version='v1',
+        description="API documentation for the User management",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('users/', views.UserListCreate.as_view(), name='user-view-create'),
     path('users/<int:pk>/', views.UserRetrieveUpdateDestroy.as_view(), name='user-view-detail'),
+
+    # Swagger URL
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
 ]
