df2d8461e941757d238726bdab13d7d88f246c61
Bảo Bùi <hongbao2003@gmail.com>
2025-02-11 16:14:51 +0700

    pagiantion

--- Ztudy/api/models.py
+++ Ztudy/api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# create simple user models
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
--- Ztudy/api/views/user_views.py
+++ Ztudy/api/views/user_views.py
@@ -1,10 +1,10 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters
-from rest_framework.response import Response
-from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
 
-from .models import User
-from .serializers import UserSerializer
+from ..models import User
+from ..pagination import CustomPagination
+from ..serializers import UserSerializer
+
 
 class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -13,6 +13,7 @@ class UserListCreate(generics.ListCreateAPIView):
     filterset_fields = ['username', 'email']
     search_fields = ['username', 'email']
     ordering_fields = ['id', 'username']
+    pagination_class = CustomPagination
 
 class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
