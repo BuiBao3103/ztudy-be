dbf250ff8993f32433141f5f6f9ab050ae3b9c00
Bảo Bùi <hongbao2003@gmail.com>
2025-02-15 16:35:09 +0700

    add .env.example

--- Ztudy/Ztudy/settings.py
+++ Ztudy/Ztudy/settings.py
@@ -38,10 +38,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # Ví dụ: Cho phép từ React app chạy trên localhost:3000
+#     "https://your-frontend-domain.com",  # Thêm các miền frontend khác nếu cần
+# ]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -61,6 +67,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
