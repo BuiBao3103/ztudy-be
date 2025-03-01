d596a4e842d313ed7bbef2a93f855b86ff81dca9
Bảo Bùi <hongbao2003@gmail.com>
2025-03-01 14:38:25 +0700

    import algorithm recommend rooms

--- Ztudy/api/views/room_views.py
+++ Ztudy/api/views/room_views.py
@@ -1,11 +1,10 @@
 from rest_flex_fields.views import FlexFieldsMixin
-from rest_framework.views import APIView
-
 from ..models import Room, RoomCategory, RoomParticipant, Interest
 from ..serializers import RoomSerializer, RoomCategorySerializer, RoomParticipantSerializer
 from .base_views import BaseListCreateView, BaseRetrieveUpdateDestroyView, SwaggerExpandMixin
-from rest_framework import generics, permissions
 from ..pagination import CustomPagination
+from django.db.models import Case, When, Value, IntegerField
+from rest_framework import generics, permissions
 
 class RoomListCreate(FlexFieldsMixin, SwaggerExpandMixin, BaseListCreateView):
     queryset = Room.objects.all()
@@ -35,6 +34,7 @@ class RoomParticipantRetrieveUpdateDestroy(FlexFieldsMixin, SwaggerExpandMixin, 
     serializer_class = RoomParticipantSerializer
     permit_list_expands = ['room', 'user']
 
+
 class SuggestedRoomsAPIView(FlexFieldsMixin, SwaggerExpandMixin, generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = RoomSerializer
@@ -42,8 +42,22 @@ class SuggestedRoomsAPIView(FlexFieldsMixin, SwaggerExpandMixin, generics.ListAP
     permit_list_expands = ['category', 'creator_user']
 
     def get_queryset(self):
-        user = self.request.user
-        interests = Interest.objects.filter(user=user).values_list('category', flat=True)
+        user = self.request.user  # Get the currently authenticated user
+        interests = Interest.objects.filter(user=user).values_list('category_id',
+                                                                   flat=True)  # Retrieve category IDs that the user is interested in
 
-        return Room.objects.filter(type="PUBLIC", category_id__in=interests, is_active=True)
+        # Assign priority: Rooms that belong to the user's interests are given priority = 1, others are assigned priority = 2
+        rooms = Room.objects.annotate(
+            priority=Case(
+                When(category_id__in=interests, then=Value(1)),
+                # Rooms matching the user's interests → Higher priority (1)
+                default=Value(2),  # Other rooms → Lower priority (2)
+                output_field=IntegerField(),
+            )
+        ).filter(type="PUBLIC", is_active=True)  # Filter only public and active rooms
+
+        # Order results: First by priority (preferred rooms first), then by newest rooms
+        return rooms.order_by("priority", "-created_at")
+
+
 
