d1c1abc4c05503288d7bdec8602b8eac7d3fb2c1
Bảo Bùi <hongbao2003@gmail.com>
2025-03-15 13:05:33 +0700

    add time recivce request

--- Ztudy/Ztudy/settings.py
+++ Ztudy/Ztudy/settings.py
@@ -285,13 +285,20 @@ CELERY_BEAT_SCHEDULE = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
         },
     },
     'loggers': {
-        '': {
+        'django': {
             'handlers': ['console'],
             'level': 'INFO',
         },
