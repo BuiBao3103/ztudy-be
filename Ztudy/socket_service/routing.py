76244cfad39ef40d90571f03ff68e31f8ea5ba16
Bảo Bùi <hongbao2003@gmail.com>
2025-04-08 00:44:06 +0700

    Refactor websocket URL patterns in routing.py to support optional leading slashes, enhancing compatibility with various frontend implementations.

--- Ztudy/socket_service/routing.py
+++ Ztudy/socket_service/routing.py
@@ -2,9 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Hỗ trợ cả URL đúng và lỗi có nhiều dấu `/`
-    re_path(r'^/?ws/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'^/?ws/online/$', consumers.OnlineStatusConsumer.as_asgi()),
-    re_path(r'^//online/$', consumers.OnlineStatusConsumer.as_asgi()),  # Chấp nhận lỗi frontend
-    re_path(r'^//chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),  # Chấp nhận lỗi frontend
+    # Hỗ trợ URL có hoặc không có dấu `/`
+    re_path(r'^/*ws/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^/*ws/online/$', consumers.OnlineStatusConsumer.as_asgi()),
 ]
