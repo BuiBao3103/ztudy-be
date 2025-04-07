57bf199b5f30deee953dbba049f3da9e6b375f92
Bảo Bùi <hongbao2003@gmail.com>
2025-04-07 23:31:23 +0700

    Remove CustomCsrfMiddleware from MIDDLEWARE in settings.py to simplify CSRF handling and enhance security configuration.

--- Ztudy/Ztudy/settings.py
+++ Ztudy/Ztudy/settings.py
@@ -130,7 +130,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'api.middleware.CustomCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
