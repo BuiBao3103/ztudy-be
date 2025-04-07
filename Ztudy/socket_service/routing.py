a4b16993181b727135a5abceb19b67be9c4ba1c8
Bảo Bùi <hongbao2003@gmail.com>
2025-04-08 00:39:41 +0700

    Enhance websocket URL patterns in routing.py to support both correct and erroneous URLs with multiple slashes, improving frontend compatibility.

--- Ztudy/socket_service/routing.py
+++ Ztudy/socket_service/routing.py
@@ -2,10 +2,9 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/online/$', consumers.OnlineStatusConsumer.as_asgi()),
-    re_path(r'/online/$', consumers.OnlineStatusConsumer.as_asgi()),
-    re_path(r'//online/$', consumers.OnlineStatusConsumer.as_asgi()),
-    re_path(r'//chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
+    # Hỗ trợ cả URL đúng và lỗi có nhiều dấu `/`
+    re_path(r'^/?ws/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^/?ws/online/$', consumers.OnlineStatusConsumer.as_asgi()),
+    re_path(r'^//online/$', consumers.OnlineStatusConsumer.as_asgi()),  # Chấp nhận lỗi frontend
+    re_path(r'^//chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),  # Chấp nhận lỗi frontend
 ]
