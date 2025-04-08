b7172208ba1a2fa9e1dcc83744f5fefa2babf6f2
Bảo Bùi <hongbao2003@gmail.com>
2025-04-09 01:06:37 +0700

    Refactor WebSocket URL patterns to remove redundant slashes
    - Updated routing.py to simplify WebSocket URL patterns by removing unnecessary leading slashes, ensuring cleaner and more consistent URL definitions for chat and online status consumers.

--- Ztudy/socket_service/routing.py
+++ Ztudy/socket_service/routing.py
@@ -2,7 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Hỗ trợ URL có hoặc không có dấu `/`
-    re_path(r'^/*ws/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'^/*ws/online/$', consumers.OnlineStatusConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/online/$', consumers.OnlineStatusConsumer.as_asgi()),
 ]
