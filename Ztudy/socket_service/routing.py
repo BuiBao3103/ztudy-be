d983559a354198dcd1b877c5155bb0bc8ed18c90
Bảo Bùi <hongbao2003@gmail.com>
2025-04-09 01:20:06 +0700

    Add WebSocket URL patterns for chat and online status consumers
    - Introduced new URL patterns in routing.py for chat and online status consumers, enhancing the WebSocket communication structure.
    - Maintained existing patterns while ensuring consistency in URL definitions.

--- Ztudy/socket_service/routing.py
+++ Ztudy/socket_service/routing.py
@@ -2,6 +2,9 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
+    re_path(r'chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'online/$', consumers.OnlineStatusConsumer.as_asgi()),
+
     re_path(r'ws/chat/(?P<code_invite>[\w-]+)/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/online/$', consumers.OnlineStatusConsumer.as_asgi()),
 ]
